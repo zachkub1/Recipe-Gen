affbef29639a0d9939ebcd07b7cb0c3440b5f309
Salahddeen Ahmed <salahuddeen.ahmed@outlook.com>
2024-07-13 13:40:10 -0400

    chore: Refactor database creation in __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -35,5 +35,5 @@ def create_app():
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+        db.create_all()
         print('Created Database!')
